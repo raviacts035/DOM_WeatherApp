06ed3dc57476194986d64b4285097b9f8a459dbf
raviacts035 <ravindrainturi035@gmail.com>
2023-04-13 17:08:58 +0530

    Update : weather app, without CSS design

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-let data;
-const inputBox=document.getElementById("")
-const countryName=document.getElementById("countryName")
-const stateName=document.getElementById("stateName")
-const cityName=document.getElementById("cityName")
-const windSpeed=document.getElementById("windSpeed")
-const temp=document.getElementById("temp")
-const Humidity=document.getElementById("Humidity")
-const ico=document.getElementById("ico")
+//defining variables and holding elements
 
+var data;
+const w_input=document.querySelector("#w_input")
+const w_country=document.querySelector("#w_country")
+const w_state=document.querySelector("#w_state")
+const w_city=document.querySelector("#w_city")
+const w_wind=document.querySelector("#w_wind")
+const w_temp=document.querySelector("#w_temp")
+const w_humidity=document.querySelector("#w_humidity")
+const w_icon=document.querySelector("#w_icon")
+
+// Fetching Data from API and asseigning to variables
+
+var getData= async (event)=>{
+    event.preventDefault();
+    if(!w_input.value){
+        alert("Please Enter city name")
+        return;
+    }
+    const city=w_input.value
+    var fetchData= await fetch(`http://api.weatherapi.com/v1/current.json?key=529cee31f2ba4da986754455231304&q=${city}`)
+    const g=await fetchData.json()
+    data =g;
+    w_country.innerHTML=data.location.country
+    w_state.innerHTML=data.location.region
+    w_city.innerHTML=data.location.name
+    w_temp.innerHTML=data.current.temp_c
+    w_humidity.innerHTML=data.current.humidity
+    w_wind.innerHTML=data.current.wind_kph
+    w_icon.src=data.current.condition.icon
+}
+// Represting Data in respective frontend field's
