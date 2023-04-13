db0cae32f932ca7f7917285868f5cdb921e52ef2
raviacts035 <ravindrainturi035@gmail.com>
2023-04-13 13:21:25 +0530

    Fist COmmit

---
+++ script.js
@@ -0,0 +1,10 @@
+let data;
+const inputBox=document.getElementById("")
+const countryName=document.getElementById("countryName")
+const stateName=document.getElementById("stateName")
+const cityName=document.getElementById("cityName")
+const windSpeed=document.getElementById("windSpeed")
+const temp=document.getElementById("temp")
+const Humidity=document.getElementById("Humidity")
+const ico=document.getElementById("ico")
+
